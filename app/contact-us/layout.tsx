02e329448d9db273c312173539f0cac54f3cdcfb
SergeyV1S <v1s.coop@yandex.ru>
2025-05-06 21:53:38 +0500

    add seo & add responsive design

--- app/contact-us/layout.tsx
+++ app/contact-us/layout.tsx
@@ -3,8 +3,48 @@ import { Box, Stack } from "@mui/material";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Igniz | Contact us",
-  description: "Contact us page"
+  title: "Igniz | Contact Us",
+  description: "Форма обратной связи для бизнес-запросов. Ответим в течение 2 рабочих часов.",
+
+  keywords: [
+    "IT решения",
+    "сайт под ключ",
+    "контакты Igniz",
+    "техподдержка",
+    "бизнес-запрос",
+    "форма обратной связи"
+  ],
+
+  openGraph: {
+    title: "Igniz | Contact Us",
+    description:
+      "Обсудите ваш проект с IT-специалистами Igniz. Заполните форму или напишите напрямую.",
+    url: "https://some-company-website.vercel.app/contact-us",
+    siteName: "Igniz Technologies",
+    locale: "ru_RU",
+    type: "website"
+  },
+
+  twitter: {
+    title: "Igniz | Contact Us",
+    description: "Форма для бизнес-запросов с гарантированным быстрым ответом",
+    site: "@igniz_tech"
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false
+  },
+  alternates: {
+    canonical: "https://some-company-website.vercel.app/contact-us"
+  },
+
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1
+  }
 };
 
 const ContactUsLayout = ({
--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,19 @@ const HomePage = () => (
   <Stack spacing={8} paddingTop={2} paddingBottom={4}>
     <Box component='section' padding={6} bgcolor='#F5F5F5'>
       <Container maxWidth='xl'>
-        <Box display='flex' alignItems='center' gap={4}>
-          <Box flexBasis='50%'>
+        <Box
+          display='flex'
+          alignItems='center'
+          gap={4}
+          sx={{
+            textAlign: { xs: "center", md: "start" },
+            flexDirection: { xs: "column", md: "row" },
+            "& > *": {
+              flexBasis: { xs: "100%", md: "50%" }
+            }
+          }}
+        >
+          <Box>
             <Box display='flex' flexDirection='column' gap={2}>
               <Typography customVariant='h1'>
                 Igniz — цифровые решения для трансформации бизнеса
@@ -20,7 +31,17 @@ const HomePage = () => (
             </Box>
           </Box>
 
-          <Box flexBasis='50%'>
+          <Box
+            sx={{
+              width: { xs: "100%", md: "auto" },
+              mt: { xs: 3, md: 0 },
+              display: "flex",
+              justifyContent: { xs: "center", md: "flex-start" },
+              "& > div": {
+                width: { xs: "100%", sm: "90%" }
+              }
+            }}
+          >
             <VideoPlayer url='https://www.youtube.com/watch?v=dQw4w9WgXcQ' />
           </Box>
         </Box>
@@ -32,7 +53,12 @@ const HomePage = () => (
         <Typography customVariant='h2' textAlign='center'>
           Наши ключевые решения
         </Typography>
-        <Grid container columns={{ xs: 4, sm: 8, md: 12 }} spacing={8}>
+        <Grid
+          container
+          columns={{ xs: 2, sm: 8, md: 12 }}
+          sx={{ textAlign: { xs: "center", sm: "start" } }}
+          spacing={8}
+        >
           {landingCards.map((card) => (
             <LandingCard key={card.title} {...card} />
           ))}
@@ -46,7 +72,9 @@ const HomePage = () => (
     <Box component='section' padding={6} borderColor='black' bgcolor='#F5F5F5'>
       <Container maxWidth='xl'>
         <Stack spacing={4} display='flex' alignItems='center'>
-          <Typography customVariant='h2'>Давайте обсудим ваш проект</Typography>
+          <Typography customVariant='h2' textAlign='center'>
+            Давайте обсудим ваш проект
+          </Typography>
           <Link href={paths.conactUs} customVariant='button'>
             Связаться
           </Link>
